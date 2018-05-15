cbd9bff92a277463c5efb171117f098dc52e7ece
terichadbourne <tchadbou@us.ibm.com>
2018-05-15 14:35:39 -0400

    unit 7 added game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardThree);
+console.log("user flipped " + cardOne);
