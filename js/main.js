bd868562b095a8b011b7807d666f19cf62cac072
terichadbourne <tchadbou@us.ibm.com>
2018-05-15 16:48:51 -0400

    unit 9 updated game logic

--- js/main.js
+++ js/main.js
@@ -2,17 +2,23 @@ console.log("up and running!")
 
 var cards = ["queen", "king", "queen", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("user flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
